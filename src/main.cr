003bc3939f79b3550861b5e0b7306fbbb5d73155
murilinhops <murilinhorps@gmail.com>
2023-10-12 22:24:49 -0400

    Add music get by id, exceptions on post and init patch

--- src/main.cr
+++ src/main.cr
@@ -1,5 +1,7 @@
 require "kemal"
 require "./models/music"
+require "./utils"
+include Utils
 
 musics = [Music.new("musíca", "artista", 1.33, album: "album not nil")]
 
@@ -14,13 +16,64 @@ get "/musics" do |env|
   musics.to_json
 end
 
+get "/musics/:id" do |env|
+  id = env.params.url["id"]
+
+  music = musics.find { |music| music.id == id }
+
+  if !music
+    error = {"message": "Error finding the music"}.to_json
+    halt env, status_code: 404, response: error
+  end
+
+  music.to_json
+end
+
+get "/musics-quantity" do |env|
+  env.response.status_code = 200
+  {"message": "There are #{musics.size} music(s)"}.to_json
+end
+
 post "/musics" do |env|
   env.response.status_code = 201
 
-  json_music = Music.from_json env.params.json.to_json
-  musics << json_music
+  begin
+    json_music = Music.from_json env.params.json.to_json
+    musics << json_music
 
-  {"message": "Música cadastrada com sucesso!"}.to_json
+    {"message": "Música cadastrada com sucesso!",
+     "ID":      "#{json_music.id}"}.to_json
+  rescue ex
+    formatted_exception = ex.message.not_nil!.split("\n")[0]
+    error = {"message": formatted_exception}.to_json
+
+    halt env, status_code: 400, response: error
+  ensure
+    Log.info { "Json received: #{env.params.json.to_json}" }.to_json
+  end
+end
+
+patch "/musics/:id" do |env|
+  music = valid_music? env.params.url["id"], musics
+  if !music
+    error = {"message": "Error finding the music"}.to_json
+    halt env, status_code: 404, response: error
+  end
+
+  json_name = env.params.json["name"]?
+  json_favorite = env.params.json["favorite"]?
+  if !json_name && !json_favorite
+    error = {"message": "No attributes to update passed to request json body"}.to_json
+    halt env, status_code: 422, response: error
+  end
+
+  music_index = musics.index! music # * save the music index before update
+  music.name = json_name.as(String)
+  music.favorite = json_favorite.as(Bool)
+  musics[music_index] = music # * update current music at its index
+
+  Log.info {"Music index: #{musics.index!(music)}"}.to_json
+  {"message": "Musica atualizada: #{music}"}.to_json
 end
 
 Kemal.run

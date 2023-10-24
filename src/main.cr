d100800bde1ac082dc830c00cca4180939f09d53
murilinhops <murilinhorps@gmail.com>
2023-10-24 19:59:33 -0300

    Finish PATCH method for favorites and name

--- src/main.cr
+++ src/main.cr
@@ -1,4 +1,5 @@
 require "kemal"
+require "json"
 require "./models/music"
 require "./utils"
 include Utils
@@ -13,13 +14,14 @@ get "/" do |env|
 end
 
 get "/musics" do |env|
+  env.response.content_type = "application/json"
   musics.to_json
 end
 
 get "/musics/:id" do |env|
   id = env.params.url["id"]
-
   music = musics.find { |music| music.id == id }
+  env.response.content_type = "application/json"
 
   if !music
     error = {"message": "Error finding the music"}.to_json
@@ -31,11 +33,13 @@ end
 
 get "/musics-quantity" do |env|
   env.response.status_code = 200
+  env.response.content_type = "application/json"
   {"message": "There are #{musics.size} music(s)"}.to_json
 end
 
 post "/musics" do |env|
   env.response.status_code = 201
+  env.response.content_type = "application/json"
 
   begin
     json_music = Music.from_json env.params.json.to_json
@@ -62,18 +66,33 @@ patch "/musics/:id" do |env|
 
   json_name = env.params.json["name"]?
   json_favorite = env.params.json["favorite"]?
+
   if !json_name && !json_favorite
     error = {"message": "No attributes to update passed to request json body"}.to_json
     halt env, status_code: 422, response: error
   end
 
   music_index = musics.index! music # * save the music index before update
-  music.name = json_name.as(String)
-  music.favorite = json_favorite.as(Bool)
-  musics[music_index] = music # * update current music at its index
+  env.response.content_type = "application/json"
+  env.response.status_code = 200
 
-  Log.info {"Music index: #{musics.index!(music)}"}.to_json
-  {"message": "Musica atualizada: #{music}"}.to_json
+  if json_name && json_favorite
+    music.name = json_name.as(String)
+    music.favorite = json_favorite.as(Bool)
+    musics[music_index] = music
+
+    {"message": "Musica atualizada: #{music.to_json}"}.to_json
+  elsif json_name
+    music.name = json_name.as(String)
+    musics[music_index] = music
+
+    {"message": "Nome da musica atualizada: #{music.name} : #{music.id}"}.to_json
+  elsif json_favorite
+    music.favorite = json_favorite.as(Bool)
+    musics[music_index] = music
+
+    {"message": "Musica é favorita?: #{music.favorite} : #{music.id}"}.to_json
+  end
 end
 
 Kemal.run
